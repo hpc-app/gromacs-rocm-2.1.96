a1697452e059bfc8aae6c8dcc0ab4c78cd286351
root <root@r4-node4>
2019-04-10 14:18:07 +0800

    add config.sh

---
+++ config.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cmake3 -DBUILD_SHARED_LIBS=ON -DGMX_BUILD_FOR_COVERAGE=on -DCMAKE_BUILD_TYPE=Debug -DGMX_CUDA_NB_SINGLE_COMPILATION_UNIT=on -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DGMX_MPI=on -DGMX_GPU=on -DGMX_OPENMP=on -DGMX_GPU_DETECTION_DONE=on -DHIP_TOOLKIT_ROOT_DIR=/opt/rocm/hip -DGMX_SIMD=AUTO -DCMAKE_INSTALL_PREFIX=/root/Desktop/gromacs-2018.4-install -DGMX_FFT_LIBRARY=fftw3 -DFFTWF_LIBRARY='/root/Desktop/fftw-3.3.8/build/lib/libfftw3f.so' -DFFTWF_INCLUDE_DIR='/root/Desktop/fftw-3.3.8/build/include'  -DBLAS_LIBRARIES='/root/Desktop/lapack-release-lapack-3.8.0/librefblas.so' -DLAPACK_LIBRARIES='/root/Desktop/lapack-release-lapack-3.8.0/liblapack.so' -DREGRESSIONTEST_DOWNLOAD=OFF -DCMAKE_PREFIX_PATH=/opt/rocm ..
